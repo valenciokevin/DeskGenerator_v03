60a037e5a7dfefb2d8786ebff9750a5b50041def
Kevin Marcos Valêncio <89864010+valenciokevin@users.noreply.github.com>
2025-02-27 10:45:57 -0300

    Update test.js

--- test/test.js
+++ test/test.js
@@ -1,3 +1,27 @@
-msg1: "ola, seu nome é 'clientName'";
-msg2: "ola 'nome' seu email é 'clientMail'";
-msg3: "ola 'nome' seu numero é 'clientPhone' e seu genero é 'clientGender'";
+function insereAtalho(msgType) {
+    // Obtendo os valores dos campos do formulário
+    const clientName = document.getElementById('clientName').value;
+    const clientPhone = document.getElementById('clientPhone').value;
+    const clientMail = document.getElementById('clientMail').value;
+    const clientGender = document.querySelector('input[name="gender"]:checked') ? document.querySelector('input[name="gender"]:checked').value : 'não especificado';
+
+    let message = '';
+
+    // Definindo a mensagem com base no tipo de atalho
+    switch (msgType) {
+        case 'msg1':
+            message = `Olá, seu nome é '${clientName}'`;
+            break;
+        case 'msg2':
+            message = `Olá '${clientName}', seu email é '${clientMail}'`;
+            break;
+        case 'msg3':
+            message = `Olá '${clientName}', seu número é '${clientPhone}' e seu gênero é '${clientGender}'`;
+            break;
+        default:
+            message = 'Mensagem não reconhecida.';
+    }
+
+    // Inserindo a mensagem no textarea
+    document.getElementById('outputMessage').value = message;
+}
